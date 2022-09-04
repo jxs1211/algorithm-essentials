6cf96fcb0c5db221b7b316d1e7915833f2838246
xian-jie.shen <327411586@qq.com>
2022-09-04 22:04:13 +0800

    add golang-versions
    Signed-off-by: xian-jie.shen <327411586@qq.com>

---
+++ src/golang-version/linear-list/array/remove-duplicates-from-sorted-array_test.go
@@ -0,0 +1,29 @@
+package algorithm
+
+import "testing"
+
+func Test_removeDuplicates(t *testing.T) {
+	tests := []struct {
+		name string
+		nums []int
+		want int
+	}{
+		{
+			name: "base",
+			nums: []int{},
+			want: 0,
+		},
+		{
+			name: "duplicate",
+			nums: []int{1, 1, 2},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeDuplicates(tt.nums); got != tt.want {
+				t.Errorf("removeDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
